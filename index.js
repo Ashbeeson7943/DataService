0e30ef9434ec0dd1d006162fd7c2c2d75eb19420
Ashley <ashbeeson@hotmail.co.uk>
2024-02-12 01:50:07 +0000

    initial setup and creation of test routes

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+//Set up express app
+const app = express();
+
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/testDB');
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.json());
+
+//init routes
+app.use('/api', require('./routes/api-test'));
+
+//Error Handling
+app.use(function (err, req, res, next) {
+    res.status(422).send({
+        Error: err.message
+    });
+});
+
+//Listen for requests
+app.listen(9000 || process.env.port, function () {
+    console.log('Now listening for requests...');
+});
